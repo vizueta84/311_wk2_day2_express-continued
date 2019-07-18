750ccc48c786c745ef011e8ba78c6df7a585b7f4
Matt Viteri <mxviteri@gmail.com>
2019-07-18 14:41:10 -0500

    removing duplicate key in products data

--- data/products.js
+++ data/products.js
@@ -2,7 +2,6 @@ module.exports = [{
     "_id": 1,
     "name": "Body Luxuries Sweet Lavender Hand Sanitizer",
     "description": "Psychotropic drugs, not elsewhere classified",
-    "reviews": 46,
     "rating": 2,
     "imgUrl": "http://dummyimage.com/136x167.bmp/cc0000/ffffff",
     "price": "$95.11",
@@ -42,7 +41,6 @@ module.exports = [{
     "_id": 2,
     "name": "Topiramate",
     "description": "Injury of ulnar nerve at forearm level, left arm, sequela",
-    "reviews": 2,
     "rating": 2,
     "imgUrl": "http://dummyimage.com/125x134.jpg/cc0000/ffffff",
     "price": "$37.09",
@@ -68,12 +66,11 @@ module.exports = [{
     "_id": 3,
     "name": "Almond",
     "description": "Other disorders of continuity of bone, unsp tibia and fibula",
-    "reviews": 27,
     "rating": 5,
     "imgUrl": "http://dummyimage.com/149x190.jpg/dddddd/000000",
     "price": "$51.83",
     "category": "food",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -88,12 +85,11 @@ module.exports = [{
     "_id": 4,
     "name": "VYTORIN",
     "description": "Orchard as the place of occurrence of the external cause",
-    "reviews": 60,
     "rating": 3,
     "imgUrl": "http://dummyimage.com/162x153.jpg/cc0000/ffffff",
     "price": "$86.93",
     "category": "electronics",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -108,12 +104,11 @@ module.exports = [{
     "_id": 5,
     "name": "Decolorized Iodine",
     "description": "Injury",
-    "reviews": 20,
     "rating": 1,
     "imgUrl": "http://dummyimage.com/120x245.jpg/cc0000/ffffff",
     "price": "$70.10",
     "category": "electronics",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -128,12 +123,11 @@ module.exports = [{
     "_id": 6,
     "name": "Fresh Sugar Honey Tinted Lip Treatment SPF15",
     "description": "Unspecified open wound ",
-    "reviews": 79,
     "rating": 3,
     "imgUrl": "http://dummyimage.com/211x227.bmp/5fa2dd/ffffff",
     "price": "$39.25",
     "category": "electronics",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -148,12 +142,11 @@ module.exports = [{
     "_id": 7,
     "name": "LBel",
     "description": "2-Propanol",
-    "reviews": 76,
     "rating": 3,
     "imgUrl": "http://dummyimage.com/212x144.jpg/ff4444/ffffff",
     "price": "$99.91",
     "category": "sporting",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -168,12 +161,11 @@ module.exports = [{
     "_id": 8,
     "name": "Cholestyramine",
     "description": "Laceration without foreign body of trachea, sequela",
-    "reviews": 74,
     "rating": 3,
     "imgUrl": "http://dummyimage.com/204x175.jpg/5fa2dd/ffffff",
     "price": "$67.17",
     "category": "sporting",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -188,12 +180,11 @@ module.exports = [{
     "_id": 9,
     "name": "Risperidone",
     "description": "cephalospor/oth beta-lactm antibiot, undet, sequela",
-    "reviews": 9,
     "rating": 1,
     "imgUrl": "http://dummyimage.com/212x108.bmp/cc0000/ffffff",
     "price": "$96.84",
     "category": "sporting",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }, {
@@ -208,12 +199,11 @@ module.exports = [{
     "_id": 10,
     "name": "MAC",
     "description": "Other Gram-negative sepsis",
-    "reviews": 45,
     "rating": 2,
     "imgUrl": "http://dummyimage.com/189x109.png/cc0000/ffffff",
     "price": "$74.37",
     "category": "sporting",
-     "reviews": [{
+    "reviews": [{
       "description": "architect revolutionary deliverables",
       "rating": 2
     }
